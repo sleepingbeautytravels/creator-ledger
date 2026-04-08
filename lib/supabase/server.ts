5eed3c27b0bde6158ad5c4f6638f67a69001a9d6
Tessa Zhang <tessazhang@Tessas-Laptop.local>
2026-04-08 21:55:41 +1000

    fix supabase typing properly

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,9 @@
 import { createServerClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 import { Database } from "@/types/database";
 
-export async function createClient() {
+export async function createClient(): Promise<SupabaseClient<Database>> {
   const cookieStore = await cookies();
 
   return createServerClient<Database>(
